89c3f6e4b02ed5d081db2994f21ba6dadc56e66a
GallosoGonzalo98 <ggalloso@alumnos.exa.unicen.edu.ar>
2023-09-22 12:24:12 -0300

    Cambiando cositas

--- app/productos.php
+++ app/productos.php
@@ -2,7 +2,14 @@
 
 function getProducto(){
     require_once 'templates/header.php';
-    echo '<h1> PRODUCTOS </h1>';
+?>
+    <div class="grid-main-products">
+        <div class="container-products">
+            <h1>PRODUCTOS</h1>
+        </div>
+    </div>
+
+<?php
     require_once 'templates/footer.php';
 }
 
